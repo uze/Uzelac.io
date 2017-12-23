47c94d3177d121df04e8e2915abef3ab45d647ba
Nikola Uzelac <nikola@uzelac.io>
2017-12-23 17:50:32 +0100

    New particle landing page

--- vendors/particles/particles.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa',
-    lineWidth: .1
-  });
-  var intro = document.getElementById('intro');
-  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
-}, false);
